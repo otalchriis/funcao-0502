d7fc4e12ae74539e2b034cc02a7647fbf0dbcad8
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-13 21:55:55 -0200

    Adicionar exemplo de Arrow Function

---
+++ arrow-function.js
@@ -0,0 +1,15 @@
+// function somar (a, b) {
+//   return a + b;
+// }
+
+// function (parametro1, parametro2) {
+//   return parametro1 + parametro1;
+// }
+
+() => {
+  return 'essa é uma arrow function anônima';
+}
+
+const somar = (a, b) => {
+  return a + b;
+}
